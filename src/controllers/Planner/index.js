7b000bad0b9fcc10192cdd6b268dde22ab802884
Vinicius <vinifladf@gmail.com>
2023-02-15 22:36:45 -0300

    Criando novos updates para planner e eventos

--- src/controllers/Planner/index.js
+++ src/controllers/Planner/index.js
@@ -155,3 +155,104 @@ export const deletEvent = async (req, res) => {
     return res.status(400).send(error.message)
   }
 }
+
+export const postCopyPlanner = async (req, res) => {
+  try {
+    const { plannerId } = req.params;
+    const { idTutor, idAluno } = req.body;
+    const planner = await Planner.findById(plannerId); 
+    const newPlanner = new Planner({
+      titulo: planner.titulo, descricao: planner.descricao, 
+      status: planner.status, idAluno, idTutor, atividades: planner.atividades
+    })
+    if(planner.titulo != null && planner.descricao != null){
+      newPlanner.save();
+      return res.json({
+        title: newPlanner.titulo,
+        description: newPlanner.descricao,
+        status: newPlanner.status,
+        idTutor: newPlanner.idTutor,
+        idAluno: newPlanner.idAluno,
+        events: newPlanner.atividades
+      })
+    }
+    else{
+      return res.status(400).json({ message: "Dados incompletos" })
+    }
+  } catch (err) {
+    return res.status(400).send(err.message)
+  }
+}
+
+export const updateEvent = async (req, res) => {
+  try {
+    const { plannerId } = req.params;
+    const { description, color, startDate, endDate, eventId} = req.body;
+
+    const planner = await Planner.findById(plannerId);
+    const events = planner.atividades;
+    var eventUpdate;
+    events.forEach(event => {
+      if(event._id.toString() == eventId){
+        eventUpdate = event;
+      }
+    });
+
+    if(eventUpdate != null && eventUpdate != undefined){
+      const newDescription = description != undefined && description != "" ? description : eventUpdate.descricao;
+      const newColor = color != undefined && color != "" ? color : eventUpdate.cor;
+      const newStartDate = startDate != undefined && startDate != "" ? startDate : eventUpdate.dtInicio;
+      const newEndDate = endDate != undefined && endDate != "" ? endDate : eventUpdate.dtFim;
+  
+      await Planner.updateOne(
+        {
+          "_id": plannerId,
+          "atividades._id": eventId
+        }, {
+        $set:
+          {
+            "atividades.$.descricao": newDescription,
+            "atividades.$.cor": newColor,
+            "atividades.$.dtInicio": newStartDate,
+            "atividades.$.dtFim": newEndDate 
+          } 
+      })
+      return res.json({ message: "Dados Atualizados com sucesso" })
+    }
+    else{
+      return res.status(400).json({message: "Atividade não encontrada"});
+    }
+
+  } catch (err) {
+    return res.status(400).send(err.message)
+  }
+}
+
+export const updatePlanner = async (req, res) => {
+  try {
+    const { plannerId } = req.params;
+    const { title, description, status} = req.body;
+
+    const planner = await Planner.findById(plannerId);
+    const newTitle = title != undefined && title != "" ? title : planner.titulo;
+    const newDescription = description != undefined && description != "" ? description : planner.descricao;
+    const newStatus = status != undefined && status != "" ? status : planner.status;
+
+    await Planner.updateOne(
+      {
+        "_id": plannerId,
+      }, {
+      $set:
+        {
+          "titulo": newTitle,
+          "descricao": newDescription,
+          "status": newStatus 
+        } 
+    })
+    return res.json({ message: "Dados Atualizados com sucesso" })
+
+  } catch (err) {
+    return res.status(400).send(err.message)
+  }
+  
+}
--- src/routes/planner.routes.js
+++ src/routes/planner.routes.js
@@ -1,14 +1,23 @@
 import { Router } from 'express';
-import {getAll, getOne, store, getAllUser, update, deletEvent} from '../controllers/Planner/index.js';
+import {getAll, getOne, store, getAllUser, 
+        update, deletEvent, postCopyPlanner,
+        updatePlanner, updateEvent} from '../controllers/Planner/index.js';
 
 const plannerRoutes = Router();
 
 plannerRoutes.get('/planners', getAll)
-plannerRoutes.post('/planner', store)
 plannerRoutes.get('/planner/:id', getOne)
 plannerRoutes.get('/planners/:idUser', getAllUser)
+
 plannerRoutes.patch('/planner', update)
+plannerRoutes.patch('/planner/:plannerId', updatePlanner);
+plannerRoutes.patch('/event/:plannerId', updateEvent)
+
 plannerRoutes.delete('/planner/event', deletEvent)
+
+plannerRoutes.post('/planner', store)
+plannerRoutes.post('/planner/:plannerId', postCopyPlanner)
+
 
 
 
